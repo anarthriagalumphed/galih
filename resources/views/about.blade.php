0b048e06ab74b2146022a0137c2eb6190357052e
anarthriagalumphed <galih22.mp@gmail.com>
2023-03-27 12:53:29 +0700

    membuat model controller

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,8 +3,8 @@
     <h1>ini about</h1>
     <!-- video ke 3 versi 1 -->
     <!-- <h3>Galih Mahendra Putra</h3>
-            <p>galihmahendrastudio@gmail.com</p>
-            <img width="100" src="img/pp.png" alt="pp"> -->
+                    <p>galihmahendrastudio@gmail.com</p>
+                    <img width="100" src="img/pp.png" alt="pp"> -->
 
 
     <!-- vid 3 ver 2 -->
@@ -17,5 +17,5 @@
 
     <h3>{{ $name }}</h3>
     <p>{{ $email }}</p>
-    <img src="{{ $image }}" alt="{{ $name }}" width="200">
+    <img class="img-thumbnail rounded-circle" src="{{ $image }}" alt="{{ $name }}" width="200">
 @endsection
